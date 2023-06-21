0026e6f5f4b1c6013da9161427659baf08bd4171
KeremAydin98 <aydinke16@itu.edu.tr>
2023-06-21 11:59:44 +0300

    initialization

---
+++ models.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+def create_model(num_characters):
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Input(shape=(100, 200)),
+        tf.keras.layers.Conv2D(128, 3, activation="relu"),
+        tf.keras.layers.MaxPooling2D(2),
+        tf.keras.layers.Conv2D(128, 3, activation="relu"),
+        tf.keras.layers.MaxPooling2D(2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(1024, activation="relu"),
+        tf.keras.layers.Dense(num_characters * 10, activation="softmax"),
+        tf.keras.layers.Reshape((num_characters, 10))
+    ])
+
+    model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                  optimizer=tf.keras.optimizers.Adam())
+    
+    return model
