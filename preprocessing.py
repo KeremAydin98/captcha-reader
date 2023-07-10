eadfc99cd43e1a75da07ff7547fad0f099d93842
aydinke16@itu.edu.tr <aydinke16@itu.edu.tr>
2023-07-10 16:44:10 +0300

    new preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -8,55 +8,51 @@ class Preprocessing():
 
     def __init__(self):
 
-        all_characters = [" "] + list(string.ascii_lowercase) + list(string.ascii_uppercase) + list(string.digits)
+      all_characters = list(string.digits) + list(string.ascii_uppercase)
 
-        self.n_characters = len(all_characters)
+      self.n_characters = len(all_characters)
 
-        label_encoder = LabelEncoder()
+      label_encoder = LabelEncoder()
 
-        encoded_characters = label_encoder.fit_transform(all_characters)
+      encoded_characters = label_encoder.fit_transform(all_characters)
 
-        self.label_to_index = {character:index for character, index in zip(all_characters, encoded_characters)}
-        self.index_to_label = {index:character for character, index in zip(all_characters, encoded_characters)}
+      self.label_to_index = {character:index for character, index in zip(all_characters, encoded_characters)}
+      self.index_to_label = {index:character for character, index in zip(all_characters, encoded_characters)}
 
     def encode_text(self, text):
 
-        return np.array([self.label_to_index[character] for character in text])
+      encodes = []
+
+      for character in text:
+
+        encoded = np.zeros(self.n_characters)
+        encoded[self.label_to_index[character]] = 1
+
+        encodes.append(encoded)
+
+      return np.array(encodes)
 
     def decode_text(self, indexes):
 
-        return [self.index_to_label[index] for index in indexes]
+      return [self.index_to_label[index] for index in indexes]
 
 def load_data(path):
 
     images = []
     labels = []
 
-    for file in os.listdir(path):
+    for filename in os.listdir(path):
 
-        if file.endswith(".png"):
+        if filename.endswith(".png"):
 
-            img = cv2.imread(os.path.join(path, file))
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            img = cv2.resize(img, (200, 100))
+            img = cv2.imread(os.path.join(path, filename),0)
+
+            # Convert source image to unsigned 8 bit integer Numpy array
             img = img / 255.0
 
-            file = file.split(".")[0]
-
-            if len(file) < 10:
-
-                label = file + (10 - len(file)) * " "
-
-            else:
-
-                label = file
+            filename = filename.split(".")[0]
 
             images.append(img)
-            labels.append(label)
+            labels.append(filename)
 
     return np.array(images), labels
-
-
-
-
-
